2214bdd4b0e460ec9fa3971574a8c8136f1ff225
ducnt1003 <nguyentrungduc0310@gmail.com>
2021-11-22 21:24:38 +0700

    fix route

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{route('admin.orders.order')}}" class="brand-link">
+    <a href="/orders/pos" class="brand-link">
         <img src="/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Rhust-app</span>
     </a>
